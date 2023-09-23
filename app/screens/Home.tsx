ce1d4be130ca6107310c2954290991ea5dc6a451
Botond Burgmann <k811qk@inf.elte.hu>
2023-09-23 13:27:53 +0200

    created exercises and fetched them

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,13 +1,38 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
-import { FIREBASE_AUTH } from '../../FirebaseConfig'
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button } from 'react-native';
+import { FIREBASE_AUTH, FIRESTORE_DB } from '../../FirebaseConfig';
+import { collection, query, getDocs } from 'firebase/firestore';
 
-const Home = () => {
+const Home: React.FC = () => {
+  const [exercises, setExercises] = useState<string[]>([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      let data: string[] = [];
+      try {
+        const exercisesCollection = collection(FIRESTORE_DB, 'Exercises');
+        const q = query(exercisesCollection);
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach(async (docSnapshot) => {
+          data.push(docSnapshot.data().name); // Assuming 'name' is the property you want to display
+        });
+        setExercises(data);
+      } catch (error: any) {
+        alert('Fetching data has failed: ' + error.message);
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Button onPress={() => FIREBASE_AUTH.signOut()} title="Log out"/>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+{/*       {exercises.map((exercise, index) => (
+        <Text key={index}>{exercise}</Text>
+      ))} */}
+      <Button onPress={() => FIREBASE_AUTH.signOut()} title="Log out" />
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
