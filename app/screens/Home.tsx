42e49adcd7d4a4b1eea278e1b5631a92d36131f3
Botond Burgmann <k811qk@inf.elte.hu>
2023-09-25 18:06:35 +0200

    Created account menu

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 import { FIREBASE_AUTH, FIRESTORE_DB } from '../../FirebaseConfig';
 import { collection, query, getDocs } from 'firebase/firestore';
 
 const Home: React.FC = () => {
-  const [exercises, setExercises] = useState<string[]>([]);
+  /* const [exercises, setExercises] = useState<string[]>([]);
 
   useEffect(() => {
     const getData = async () => {
@@ -23,16 +23,52 @@ const Home: React.FC = () => {
     };
 
     getData();
-  }, []);
+  }, []); */
+
+  const [accountContainerStyle, setAccountContainerStyle] = useState(false);
+
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-{/*       {exercises.map((exercise, index) => (
-        <Text key={index}>{exercise}</Text>
-      ))} */}
-      <Button onPress={() => FIREBASE_AUTH.signOut()} title="Log out" />
+    <View style={styles.container}>
+      
+      <View style={accountContainerStyle ? styles.accountContainerShown : styles.accountContainerHidden}>
+        <Button  title="Profile Settings" />
+        <Button onPress={() => FIREBASE_AUTH.signOut()} title="Log out" />
+      </View>
+      <View style={styles.navbar}><Button onPress={() => setAccountContainerStyle(!accountContainerStyle)} title="Account" /></View>
     </View>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  navbar:{
+    
+  },
+  container:{
+    flex: 1, 
+    justifyContent: 'flex-end', 
+    alignItems: 'flex-end' 
+  },
+  accountContainerHidden:{
+    display: 'none',
+    height: '100%',
+    width: '50%',
+    backgroundColor: '#1E2D3E',
+    flex: 1, 
+    justifyContent: 'flex-end', 
+    alignItems: 'flex-start', 
+    paddingBottom: 30
+  },
+  accountContainerShown:{
+    display: 'flex',
+    height: '100%',
+    width: '50%',
+    backgroundColor: '#1E2D3E',
+    flex: 1, 
+    justifyContent: 'flex-end', 
+    alignItems: 'flex-start', 
+    paddingBottom: 30
+  }
+});
