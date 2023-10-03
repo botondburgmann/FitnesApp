57d02759d67301d06f7cfe24e675f55d5490c8b5
Botond Burgmann <k811qk@inf.elte.hu>
2023-10-03 20:00:46 +0200

    can Add unilateral set now

--- app/components/UnilateralSet.tsx
+++ app/components/UnilateralSet.tsx
@@ -1,50 +1,75 @@
 import { View, Text, TextInput } from 'react-native'
 import React from 'react'
 
-const UnilateralSet = () => {
-  return (
-    <View>
-        <TextInput
-            keyboardType='numeric'
-            placeholder='Weight (kg) left'
-            autoCapitalize='none'
-        />
-        <TextInput
-            keyboardType='numeric'
-            placeholder='Reps left'
-            autoCapitalize='none'
-        />
-        <TextInput
-            keyboardType='numeric'
-            placeholder='Time (seconds) left'
-            autoCapitalize='none'
-        />
-        <TextInput
-            keyboardType='numeric'
-            placeholder='Rest time (seconds) left'
-            autoCapitalize='none'
-        />       
-        <TextInput
-            keyboardType='numeric'
-            placeholder='Weight (kg) right'
-            autoCapitalize='none'
-        />
-        <TextInput
-            keyboardType='numeric'
-            placeholder='Reps right'
-            autoCapitalize='none'
-        />
-        <TextInput
-            keyboardType='numeric'
-            placeholder='Time (seconds) right'
-            autoCapitalize='none'
-        />
-        <TextInput
-            keyboardType='numeric'
-            placeholder='Rest time (seconds) right'
-            autoCapitalize='none'
-        />       
-    </View>
+const UnilateralSet = (props) => {
+    const {leftWeight, setLeftWeight} = props.leftWeight;    
+    const {leftReps, setLeftReps} = props.leftReps;    
+    const {leftTime, setLeftTime} = props.leftTime;    
+    const {leftRestTime, setLeftRestTime} = props.leftRestTime;    
+    const {rightWeight, setRightWeight} = props.rightWeight;    
+    const {rightReps, setRightReps} = props.rightReps;    
+    const {rightTime, setRightTime} = props.rightTime;    
+    const {rightRestTime, setRightRestTime} = props.rightRestTime;    
+
+    return (
+        <View>
+            <TextInput
+                keyboardType='numeric'
+                value={leftWeight}
+                onChangeText={(text) => setLeftWeight(text)}
+                placeholder='Weight (kg) left'
+                autoCapitalize='none'
+            />
+            <TextInput
+                keyboardType='numeric'
+                value={leftReps}
+                onChangeText={(text) => setLeftReps(text)}
+                placeholder='Reps left'
+                autoCapitalize='none'
+            />
+            <TextInput
+                keyboardType='numeric'
+                value={leftTime}
+                onChangeText={(text) => setLeftTime(text)}
+                placeholder='Time (seconds) left'
+                autoCapitalize='none'
+            />
+            <TextInput
+                keyboardType='numeric'
+                value={leftRestTime}
+                onChangeText={(text) => setLeftRestTime(text)}
+                placeholder='Rest time (seconds) left'
+                autoCapitalize='none'
+            />       
+            <TextInput
+                keyboardType='numeric'
+                value={rightWeight}
+                onChangeText={(text) => setRightWeight(text)}
+                placeholder='Weight (kg) right'
+                autoCapitalize='none'
+            />
+            <TextInput
+                keyboardType='numeric'
+                value={rightReps}
+                onChangeText={(text) => setRightReps(text)}
+                placeholder='Reps right'
+                autoCapitalize='none'
+            />
+            <TextInput
+                keyboardType='numeric'
+                value={rightTime}
+                onChangeText={(text) => setRightTime(text)}
+                placeholder='Time (seconds) right'
+                autoCapitalize='none'
+            />
+            <TextInput
+                keyboardType='numeric'
+                value={rightRestTime}
+                onChangeText={(text) => setRightRestTime(text)}
+                placeholder='Rest time (seconds) right'
+                autoCapitalize='none'
+            />       
+        </View>
   )
 }
 
--- app/screens/AddWorkout.tsx
+++ app/screens/AddWorkout.tsx
@@ -1,10 +1,10 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, {useState } from 'react'
 import DateTimePicker from '@react-native-community/datetimepicker';
 import DropDownPicker from 'react-native-dropdown-picker';
 import BilateralSet from '../components/BilateralSet';
 import UnilateralSet from '../components/UnilateralSet';
-import { addExercise, getExercises } from '../functions/databaseQueries';
+import { addExercise } from '../functions/databaseQueries';
 
 interface RouteParams {
   userID: string;
@@ -14,16 +14,27 @@ interface RouteParams {
 const AddWorkout = () => {
     const [date, setDate] = useState(new Date());
     const [showDate, setShowDate] = useState(false);
+
+    // For bilateral sets
     const [weight, setWeight] = useState("");
     const [reps, setReps] = useState("");
     const [time, setTime] = useState("");
     const [restTime, setRestTime] = useState("");
 
+    // For unilateral sets
+    const [leftWeight, setLeftWeight] = useState("");
+    const [leftReps, setLeftReps] = useState("");
+    const [leftTime, setLeftTime] = useState("");
+    const [leftRestTime, setLeftRestTime] = useState("");
+    const [rightWeight, setRightWeight] = useState("");
+    const [rightReps, setRightReps] = useState("");
+    const [rightTime, setRightTime] = useState("");
+    const [rightRestTime  , setRightRestTime] = useState("");
+
     const [showSelect, setShowSelect] = useState(false);
     const [open, setOpen] = useState(false);
     const [value, setValue] = useState(null);
 
-    //getExercises(userID)
 
     const [exercises, setExercises] = useState([
         {label: 'Bench press', value: 'Bench Press'},
@@ -41,7 +52,32 @@ const AddWorkout = () => {
         setShowSelect(true)
     }
 
-    function handleAddButtonPress(exercises: (string | Array<string>), weight: Number, reps: Number, time: Number, restTime: Number) {
+    function addUnilateralSet(exercises: (string | Array<string>), leftWeight: Number, leftReps: Number, leftTime: Number, leftRestTime: Number, 
+                                                                  rightWeight: Number, rightReps: Number, rightTime: Number, rightRestTime: Number) {
+      setShowSelect(false); 
+      setValue(null);
+      const sets = {
+        "leftWeights" : [leftWeight],
+        "leftReps" : [leftReps],
+        "leftTime" : [leftTime],
+        "leftRestTime" : [leftRestTime],
+        "rightWeights" : [rightWeight],
+        "rightReps" : [rightReps],
+        "rightTime" : [rightTime],
+        "rightRestTime" : [rightRestTime]
+      }
+      addExercise("sdfdfsf", date, exercises, sets);
+      setLeftWeight("");
+      setLeftReps("");
+      setLeftTime("");
+      setLeftRestTime("");
+      setRightWeight("");
+      setRightReps("");
+      setRightTime("");
+      setRightRestTime("");
+    }
+
+    function addBilateralSet(exercises: (string | Array<string>), weight: Number, reps: Number, time: Number, restTime: Number) {
       setShowSelect(false); 
       setValue(null);
       const sets = {
@@ -61,50 +97,59 @@ const AddWorkout = () => {
     <View style={styles.container}>
       <Text>Select date</Text>
       <Pressable style={styles.button} onPress={() => setShowDate(true)}>
-                <Text style={styles.text}>Show date picker!</Text>
-        </Pressable>
-        {showDate && (
-                <DateTimePicker
-                testID="dateTimePicker"
-                value={date}
-                mode='date'
-                onChange={(event, selectedDate) => onChange(selectedDate)}
-                />
-            )}
-        <Pressable style={styles.button} onPress={handleNewExerciseButtonPress}>
-                <Text style={styles.text}>Add new exercise</Text>
-        </Pressable>
-        {showSelect && (
-                <DropDownPicker
-                open={open}
-                value={value}
-                items={exercises}
-                setOpen={setOpen}
-                setValue={setValue}
-                setItems={setExercises} />
-                )}
-                {value === 'Lunge'? 
-                  <>
-                      <UnilateralSet />
-                      <Pressable style={styles.button} onPress={()=>{setShowSelect(false); setValue(null)}}>
-                            <Text style={styles.text}>Add</Text>
-                      </Pressable>
-                  </>
-                : value === 'Bench Press' ?
-                  <>
-                      <BilateralSet 
-                        weight={{ weight: weight, setWeight: setWeight }}
-                        reps={{ reps: reps, setReps: setReps }}
-                        time={{ time: time, setTime: setTime }}
-                        restTime={{ restTime: restTime, setRestTime: setRestTime }}
-                      
-                      />
-                      <Pressable style={styles.button} onPress={() => handleAddButtonPress(value, Number(weight), Number(reps), Number(time), Number(restTime))}>
-                          <Text style={styles.text}>Add</Text>
-                      </Pressable>
-                  </>
-                  : <></>
-                                }
+        <Text style={styles.text}>Show date picker!</Text>
+      </Pressable>
+      {showDate && (
+      <DateTimePicker
+        testID="dateTimePicker"
+        value={date}
+        mode='date'
+        onChange={(event, selectedDate) => onChange(selectedDate)}
+      />
+      )}
+      <Pressable style={styles.button} onPress={handleNewExerciseButtonPress}>
+        <Text style={styles.text}>Add new exercise</Text>
+      </Pressable>
+      {showSelect && (
+      <DropDownPicker
+        open={open}
+        value={value}
+        items={exercises}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setExercises} />
+      )}
+      {value === 'Lunge' ? 
+        <>
+          <UnilateralSet
+                        leftWeight={{ leftWeight: leftWeight, setLeftWeight: setLeftWeight }}
+                        leftReps={{ leftReps: leftReps, setLeftReps: setLeftReps }}
+                        leftTime={{ leftTime: leftTime, setLeftTime: setLeftTime }}
+                        leftRestTime={{ leftRestTime: leftRestTime, setLeftRestTime: setLeftRestTime }} 
+                        rightWeight={{ rightWeight: rightWeight, setRightWeight: setRightWeight }}
+                        rightReps={{ rightReps: rightReps, setRightReps: setRightReps }}
+                        rightTime={{ rightTime: rightTime, setTime: setTime }}
+                        rightRestTime={{ rightRestTime: rightRestTime, setRightRestTime: setRightRestTime }} 
+          />
+          <Pressable style={styles.button} onPress={() => addUnilateralSet(value, Number(leftWeight), Number(leftReps), Number(leftTime), Number(leftRestTime),
+                                                                            Number(rightWeight), Number(rightReps), Number(rightTime), Number(rightRestTime))}>
+            <Text style={styles.text}>Add</Text>
+          </Pressable>
+        </>
+      : value === 'Bench Press' ?
+        <>
+          <BilateralSet 
+            weight={{ weight: weight, setWeight: setWeight }}
+            reps={{ reps: reps, setReps: setReps }}
+            time={{ time: time, setTime: setTime }}
+            restTime={{ restTime: restTime, setRestTime: setRestTime }} 
+          />
+          <Pressable style={styles.button} onPress={() => addBilateralSet(value, Number(weight), Number(reps), Number(time), Number(restTime))}>
+            <Text style={styles.text}>Add</Text>
+          </Pressable>
+        </>
+      : <></>
+      } 
     </View>
   )
 }
