6d22fb7d700abfb71ea36a576a9fa8b61268ff91
Botond Burgmann <k811qk@inf.elte.hu>
2023-10-23 18:07:18 +0200

    Recommendation works

--- app/screens/CurrentExercise.tsx
+++ app/screens/CurrentExercise.tsx
@@ -25,7 +25,7 @@ const CurrentExercise = ({route}) => {
     'Upper body': ['Abs', 'Obliques', 'Chest', 'Lower back', 'Upper back', 'Traps', 'Lats', 'Front delts', 'Middle delts', 
                   'Rear delts', 'Biceps', 'Triceps', 'Forearms'],
     'Full body pull' : ['Glutes', 'Hamstrings', 'Lower back', 'Upper back', 'Traps', 'Lats', 'Rear delts', 'Biceps',  'Forearms'],
-    'Full push pull' : ['Calves', 'Quadriceps', 'Chest', 'Front delts',  'Middle delts', 'Triceps'],
+    'Full body push' : ['Calves', 'Quadriceps', 'Chest', 'Front delts',  'Middle delts', 'Triceps'],
   }
 
   const {data:exercises, isPending:exercisesPending, error:exercisesError } = useFetch(getExercisesByFocus, userID, muscles[workoutType]);
@@ -33,12 +33,16 @@ const CurrentExercise = ({route}) => {
   const whatToTrain = [];
 
   if (exercises) {
-    
+    const workout = {
+      exercises: [],
+      numberOfSets: [],
+      numberOfReps: [],
+    };
     if (focus === "Strength") {
-      createStrengthTraining(exercises)
+      createStrengthTraining(exercises, workout)
     }
     else if (focus === "Hypertrophy")
-      createHypertrophyTraining(exercises);
+      createHypertrophyTraining(exercises, workout);
 
   }
 
@@ -50,12 +54,8 @@ const CurrentExercise = ({route}) => {
     }
   }
   
-  function createStrengthTraining(exercises) {
-    const workout = {
-      exercises: [],
-      numberOfSets: [],
-      numberOfReps: [],
-    };
+  function createStrengthTraining(exercises, workout) {
+
     let numberOfRemainingExercises = 3;
     for (const exercise of exercises) {
       if (exercise.musclesWorked.length > 2 && numberOfRemainingExercises > 0) {
@@ -64,7 +64,7 @@ const CurrentExercise = ({route}) => {
           workout.exercises.push(exercise);
           const numberOfSet =  Math.floor(Math.random() * (3 - 1 + 1)) + 1;
           workout.numberOfSets.push(numberOfSet);
-          const numberOfReps =  Math.floor(Math.random() * (5 - 1 + 1)) +1 ;
+          const numberOfReps =  Math.floor(Math.random() * (4 - 2 + 1)) + 2 ;
           workout.numberOfReps.push(numberOfReps);
           numberOfRemainingExercises--;
         }
@@ -74,29 +74,51 @@ const CurrentExercise = ({route}) => {
       alert("Can't create strength training for this type")
     }
     for (let i = 0; i < workout.exercises.length; i++) {
-      whatToTrain.push(<Text key={workout.exercises[i].name}>{workout.exercises[i].name} {workout.numberOfSets[i]} sets for {workout.numberOfReps[i]} reps</Text>);
+      whatToTrain.push(<Text key={workout.exercises[i].name}>{workout.exercises[i].name} {workout.numberOfSets[i]} sets for {workout.numberOfReps[i]-1}-{workout.numberOfReps[i]+1} reps</Text>);
       
     }
 
   }
 
-  function createHypertrophyTraining(exercises) {
-    const workout = {
-      exercises: [],
-      numberOfSets: [],
-      numberOfReps: [],
-    };
+  function createHypertrophyTraining(exercises, workout) {
     shuffleArray(exercises)
     
-    for (const exercise of exercises) {
-      const result = exercise.musclesWorked.some(muscle => muscles[workoutType].includes(muscle));      
-      
-      if (result) {
-        workout.exercises.push(exercise);
-        whatToTrain.push(<Text key={exercise.name}>{exercise.name}  {exercise.musclesWorked} </Text>);
-        muscles[workoutType] = muscles[workoutType].filter((muscle) => !exercise.musclesWorked.includes(muscle))
+    if (muscles[workoutType].length > 1) {
+      for (const exercise of exercises) {
+        const result = exercise.musclesWorked.some(muscle => muscles[workoutType].includes(muscle));      
+        
+        if (result) {
+          workout.exercises.push(exercise);
+          const numberOfSet =  Math.floor(Math.random() * (5 - 3 + 1)) + 3;
+          workout.numberOfSets.push(numberOfSet);
+          const numberOfReps =  Math.floor(Math.random() * (8 - 7 + 1)) + 7 ;
+          workout.numberOfReps.push(numberOfReps);
+          whatToTrain.push(<Text key={exercise.name}>{exercise.name}  {numberOfSet} sets for {numberOfReps-1}-{numberOfReps+1} reps </Text>);
+          muscles[workoutType] = muscles[workoutType].filter((muscle) => !exercise.musclesWorked.includes(muscle))          
+        
+  
+        } 
+    }
+    }
+    else{
+      let numberOfRemainingExercises = 6;
 
-      } 
+    //  for (let i = 0; i < 6; i++) {
+        for (const exercise of exercises) {
+          const result = exercise.musclesWorked.some(muscle => muscles[workoutType].includes(muscle));      
+          
+          if (result && numberOfRemainingExercises > 0) {
+            workout.exercises.push(exercise);
+            const numberOfSet =  Math.floor(Math.random() * (5 - 3 + 1)) + 3;
+            workout.numberOfSets.push(numberOfSet);
+            const numberOfReps =  Math.floor(Math.random() * (8 - 7 + 1)) + 7 ;
+            workout.numberOfReps.push(numberOfReps);
+            whatToTrain.push(<Text key={exercise.name}>{exercise.name}  {numberOfSet} sets for {numberOfReps-1}-{numberOfReps+1} reps </Text>);
+            numberOfRemainingExercises--;
+    
+          }
+        }
+     // }
     }
 
  /*    for (const exercise of exercises) {
