5e711e2bfeebcd85d8aeda83b4accf8b1ac745d7
Botond Burgmann <k811qk@inf.elte.hu>
2023-12-06 10:28:23 +0100

    removed some unnecessarry try catch

--- app/functions/globalFunctions.ts
+++ app/functions/globalFunctions.ts
@@ -1,27 +1,20 @@
 export function validateBirthday(birthDate:Date): void {
-  try {
-    const today = new Date();
-    
-    if (today.getFullYear() - birthDate.getFullYear() < 12) 
-      throw new Error("You need to be at least 12 years old to use this application");
-    if (today.getFullYear() - birthDate.getFullYear() > 120) 
-      throw new Error(`Are you sure you're ${today.getFullYear() - birthDate.getFullYear()} years old?`);
-    if (today.getFullYear() - birthDate.getFullYear() < 0) 
-      throw new Error("Unfortunately this time we cannot sign up time travellers. Sorry for the inconvenience");
-  } catch (error: any) {
-    alert(`Invalid birthday: ${error.message}`);
-  }
+  const today = new Date();
+  
+  if (today.getFullYear() - birthDate.getFullYear() < 12) 
+    throw new Error("Invalid birthday: You need to be at least 12 years old to use this application");
+  if (today.getFullYear() - birthDate.getFullYear() > 120) 
+    throw new Error(`Invalid birthday: Are you sure you're ${today.getFullYear() - birthDate.getFullYear()} years old?`);
+  if (today.getFullYear() - birthDate.getFullYear() < 0) 
+    throw new Error("Invalid birthday: Unfortunately this time we cannot sign up time travellers. Sorry for the inconvenience");
+
 }
 
 export function validateWeight(weight: number): void {
-  try {
-    if (Number.isNaN(weight)) 
-      throw new Error("Weight must be set");
-    if (weight < 0) 
-      throw new Error("Weight must be a positive number");
-  } catch (error: any) {
-    alert(`Invalid weight: ${error.message}`);
-  }
+  if (Number.isNaN(weight)) 
+    throw new Error("Invalid weight: Weight must be set");
+  if (weight < 0) 
+    throw new Error("Invalid weight: Weight must be a positive number");
 }
 
 export function convertLbsToKg(weight:number): number {
@@ -29,14 +22,10 @@ export function convertLbsToKg(weight:number): number {
 }
 
 export function validateHeight(height: number): void {
-  try {
-    if (Number.isNaN(height)) 
-      throw new Error("Weight must be set");
-    if (height < 0) 
-      throw new Error("Weight must be a positive number");
-  } catch (error: any) {
-    alert(`Invalid height: ${error.message}`);
-  }
+  if (Number.isNaN(height))   
+    throw new Error("Invalid height: Weight must be set");
+  if (height < 0) 
+    throw new Error("Invalid height: Weight must be a positive number");
 }
 
 export function convertFtToCm(height:number): number {
