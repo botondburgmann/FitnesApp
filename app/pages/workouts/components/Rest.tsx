326c2a4a523f0f46cda8a1fb2340e7d557f5122f
Botond Burgmann <k811qk@inf.elte.hu>
2024-04-05 10:47:28 +0200

    Removed timer for testing

--- app/pages/workouts/components/Rest.tsx
+++ app/pages/workouts/components/Rest.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React, { useContext, useRef } from 'react'
 import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
 import { Exercise, Sets, SingleSet } from '../../../types and interfaces/types'
@@ -18,10 +18,49 @@ const Rest = (props: { setCompletedSet: Function; sets:  React.MutableRefObject<
   const isLastSetOfExercise = props.isLastSetOfExercise;
   const duration = exercise.musclesWorked.length > 1 ? 180 : 120;
 
+  function completeRest() {
+    {
+      const lastSet: SingleSet = {
+        exercise: sets.current.exercise[sets.current.exercise.length-1],
+        reps: sets.current.reps[sets.current.reps.length-1],
+        restTime: sets.current.restTimes[sets.current.restTimes.length-1],
+        side: sets.current.sides[sets.current.sides.length-1],
+        time: sets.current.times[sets.current.times.length-1],
+        weight: sets.current.weights[sets.current.weights.length-1]
+      }
+      if (exercise.unilateral) {
+        sets.current.restTimes.push(...[0, duration]);
+
+        totalXP.current += addXP(exercise.isometric, lastSet)
+        totalXP.current += addXP(exercise.isometric, lastSet)
+      }
+      else {
+        sets.current.restTimes.push(duration);
+
+        totalXP.current += addXP(exercise.isometric, lastSet)
+      }
+      setCompletedSet(false)
+      if (isLastSetOfExercise && userID && week) {            
+        finishExercise(sets.current, userID, new Date(), week, totalXP.current );
+        sets.current = {
+          exercise : [],
+          dates: [],
+          weights: [],
+          reps: [],
+          times: [],
+          restTimes: [],
+          sides: []
+        }
+        totalXP.current = 0;
+      }
+      
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text style={[globalStyles.label, {marginVertical: 50}]}>Rest</Text>
-      <CountdownCircleTimer
+      {/* <CountdownCircleTimer
         isPlaying
         duration={duration}
         colors={['#FF0000', '#FF0000', '#FF0000', '#FF0000']}
@@ -64,7 +103,10 @@ const Rest = (props: { setCompletedSet: Function; sets:  React.MutableRefObject<
         }}
       >
         {({ remainingTime }) =><Text style={[globalStyles.text, {textTransform: "uppercase", fontWeight: "600", paddingVertical: 10}]}>{Math.floor(remainingTime / 60)}:{remainingTime % 60}</Text>}
-      </CountdownCircleTimer>
+      </CountdownCircleTimer> */}
+              <Pressable style={[globalStyles.button, {width: 100}]} onPress={() => completeRest()}>
+            <Text style={globalStyles.buttonText}>Next</Text>                   
+        </Pressable>
     </View>
   )
 }
